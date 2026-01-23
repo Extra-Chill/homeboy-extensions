55903c6098d1537f1907b18537b827c9a6df3b2c
Chris Huber <chubes4@users.noreply.github.com>
2026-01-22 23:14:12 -0600

    fix: add missing WordPress function stubs to validate-autoload.php
    Add stubs for common WordPress functions called at plugin load time:
    - Options API (get_option, get_site_option, update_option, etc.)
    - Multisite (is_multisite, switch_to_blog, restore_current_blog)
    - Caching (wp_cache_get, wp_cache_set, etc.)
    - Hook state checks (did_action, doing_action, has_filter, etc.)
    - User/Auth (get_current_user_id, current_user_can, etc.)
    - i18n (__, _e, esc_html__, etc.)
    - Escaping (esc_html, esc_attr, esc_url, esc_sql)
    - Sanitization (wp_kses_post, sanitize_text_field)
    - Utilities (absint, wp_parse_args, wp_json_encode, wp_unslash)
    - Constants (DOING_AJAX, DOING_CRON, WP_DEBUG)
    
    Fixes #7

--- wordpress/scripts/validate-autoload.php
+++ wordpress/scripts/validate-autoload.php
@@ -103,6 +103,296 @@ if (!function_exists('is_admin')) {
     }
 }
 
+// Options API stubs
+if (!function_exists('get_option')) {
+    function get_option($option, $default = false) {
+        return $default;
+    }
+}
+if (!function_exists('get_site_option')) {
+    function get_site_option($option, $default = false) {
+        return $default;
+    }
+}
+if (!function_exists('update_option')) {
+    function update_option($option, $value, $autoload = null) {
+        return true;
+    }
+}
+if (!function_exists('update_site_option')) {
+    function update_site_option($option, $value) {
+        return true;
+    }
+}
+if (!function_exists('delete_option')) {
+    function delete_option($option) {
+        return true;
+    }
+}
+if (!function_exists('delete_site_option')) {
+    function delete_site_option($option) {
+        return true;
+    }
+}
+if (!function_exists('add_option')) {
+    function add_option($option, $value = '', $deprecated = '', $autoload = 'yes') {
+        return true;
+    }
+}
+if (!function_exists('add_site_option')) {
+    function add_site_option($option, $value) {
+        return true;
+    }
+}
+
+// Multisite stubs
+if (!function_exists('is_multisite')) {
+    function is_multisite() {
+        return false;
+    }
+}
+if (!function_exists('get_current_blog_id')) {
+    function get_current_blog_id() {
+        return 1;
+    }
+}
+if (!function_exists('get_current_network_id')) {
+    function get_current_network_id() {
+        return 1;
+    }
+}
+if (!function_exists('get_network_option')) {
+    function get_network_option($network_id, $option, $default = false) {
+        return $default;
+    }
+}
+if (!function_exists('switch_to_blog')) {
+    function switch_to_blog($blog_id, $deprecated = null) {
+        return true;
+    }
+}
+if (!function_exists('restore_current_blog')) {
+    function restore_current_blog() {
+        return true;
+    }
+}
+
+// Caching stubs
+if (!function_exists('wp_cache_get')) {
+    function wp_cache_get($key, $group = '', $force = false, &$found = null) {
+        $found = false;
+        return false;
+    }
+}
+if (!function_exists('wp_cache_set')) {
+    function wp_cache_set($key, $data, $group = '', $expire = 0) {
+        return true;
+    }
+}
+if (!function_exists('wp_cache_add')) {
+    function wp_cache_add($key, $data, $group = '', $expire = 0) {
+        return true;
+    }
+}
+if (!function_exists('wp_cache_delete')) {
+    function wp_cache_delete($key, $group = '') {
+        return true;
+    }
+}
+
+// Hook state check stubs
+if (!function_exists('did_action')) {
+    function did_action($hook_name) {
+        return 0;
+    }
+}
+if (!function_exists('doing_action')) {
+    function doing_action($hook_name = null) {
+        return false;
+    }
+}
+if (!function_exists('doing_filter')) {
+    function doing_filter($hook_name = null) {
+        return false;
+    }
+}
+if (!function_exists('current_filter')) {
+    function current_filter() {
+        return '';
+    }
+}
+if (!function_exists('has_action')) {
+    function has_action($hook_name, $callback = false) {
+        return false;
+    }
+}
+if (!function_exists('has_filter')) {
+    function has_filter($hook_name, $callback = false) {
+        return false;
+    }
+}
+if (!function_exists('remove_action')) {
+    function remove_action($hook_name, $callback, $priority = 10) {
+        return true;
+    }
+}
+if (!function_exists('remove_filter')) {
+    function remove_filter($hook_name, $callback, $priority = 10) {
+        return true;
+    }
+}
+if (!function_exists('do_action')) {
+    function do_action($hook_name, ...$args) {
+        return;
+    }
+}
+if (!function_exists('do_action_ref_array')) {
+    function do_action_ref_array($hook_name, $args) {
+        return;
+    }
+}
+if (!function_exists('apply_filters')) {
+    function apply_filters($hook_name, $value, ...$args) {
+        return $value;
+    }
+}
+if (!function_exists('apply_filters_ref_array')) {
+    function apply_filters_ref_array($hook_name, $args) {
+        return $args[0] ?? null;
+    }
+}
+
+// User/Auth stubs
+if (!function_exists('get_current_user_id')) {
+    function get_current_user_id() {
+        return 0;
+    }
+}
+if (!function_exists('is_user_logged_in')) {
+    function is_user_logged_in() {
+        return false;
+    }
+}
+if (!function_exists('current_user_can')) {
+    function current_user_can($capability, ...$args) {
+        return false;
+    }
+}
+if (!function_exists('wp_get_current_user')) {
+    function wp_get_current_user() {
+        return (object) ['ID' => 0, 'user_login' => '', 'user_email' => ''];
+    }
+}
+
+// i18n stubs
+if (!function_exists('__')) {
+    function __($text, $domain = 'default') {
+        return $text;
+    }
+}
+if (!function_exists('_e')) {
+    function _e($text, $domain = 'default') {
+        echo $text;
+    }
+}
+if (!function_exists('esc_html__')) {
+    function esc_html__($text, $domain = 'default') {
+        return $text;
+    }
+}
+if (!function_exists('esc_attr__')) {
+    function esc_attr__($text, $domain = 'default') {
+        return $text;
+    }
+}
+if (!function_exists('_n')) {
+    function _n($single, $plural, $number, $domain = 'default') {
+        return ($number == 1) ? $single : $plural;
+    }
+}
+if (!function_exists('_x')) {
+    function _x($text, $context, $domain = 'default') {
+        return $text;
+    }
+}
+
+// Escaping stubs
+if (!function_exists('esc_html')) {
+    function esc_html($text) {
+        return $text;
+    }
+}
+if (!function_exists('esc_attr')) {
+    function esc_attr($text) {
+        return $text;
+    }
+}
+if (!function_exists('esc_url')) {
+    function esc_url($url, $protocols = null, $_context = 'display') {
+        return $url;
+    }
+}
+if (!function_exists('esc_sql')) {
+    function esc_sql($data) {
+        return $data;
+    }
+}
+
+// Sanitization stubs
+if (!function_exists('wp_kses_post')) {
+    function wp_kses_post($data) {
+        return $data;
+    }
+}
+if (!function_exists('sanitize_text_field')) {
+    function sanitize_text_field($str) {
+        return $str;
+    }
+}
+
+// Utility stubs
+if (!function_exists('absint')) {
+    function absint($maybeint) {
+        return abs((int) $maybeint);
+    }
+}
+if (!function_exists('wp_parse_args')) {
+    function wp_parse_args($args, $defaults = array()) {
+        if (is_object($args)) {
+            $parsed_args = get_object_vars($args);
+        } elseif (is_array($args)) {
+            $parsed_args = $args;
+        } else {
+            parse_str($args, $parsed_args);
+        }
+        return array_merge($defaults, $parsed_args);
+    }
+}
+if (!function_exists('wp_json_encode')) {
+    function wp_json_encode($data, $options = 0, $depth = 512) {
+        return json_encode($data, $options, $depth);
+    }
+}
+if (!function_exists('wp_unslash')) {
+    function wp_unslash($value) {
+        if (is_array($value)) {
+            return array_map('wp_unslash', $value);
+        }
+        return stripslashes($value);
+    }
+}
+
+// Define common constants if not defined
+if (!defined('DOING_AJAX')) {
+    define('DOING_AJAX', false);
+}
+if (!defined('DOING_CRON')) {
+    define('DOING_CRON', false);
+}
+if (!defined('WP_DEBUG')) {
+    define('WP_DEBUG', false);
+}
+
 // Handle based on component type
 if ($component['type'] === 'theme') {
     // Add theme-specific stubs
